aa983d3c73bdf6dfccdc1f4adde4c3320fa30d22
高桥凉介 <1979836005@qq.com>
2025-11-30 13:23:01 +0800

    ract-router init

---
+++ note-fronted/src/pages/CanvasPage.tsx
@@ -0,0 +1,7 @@
+/**
+ * 画布页面
+ * @constructor
+ */
+export default function CanvasPage() {
+    return  <h2>画板编辑器页面（Excalidraw）</h2>;
+}
---
+++ note-fronted/src/pages/DocPage.tsx
@@ -0,0 +1,7 @@
+/**
+ * 文档编辑器页面
+ * @constructor
+ */
+export default function DocPage() {
+    return <h2>文档编辑器页面（MilkDown）</h2>;
+}
