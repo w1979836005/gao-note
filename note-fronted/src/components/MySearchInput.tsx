fdf09505adaab8051badad9d8d5f8ce2e27cc496
高桥凉介 <1979836005@qq.com>
2025-11-30 18:39:01 +0800

    首页基础完成

--- note-fronted/src/components/MySearchInput.tsx
+++ note-fronted/src/components/MySearchInput.tsx
@@ -2,8 +2,14 @@
  * 搜索框组件
  * @constructor
  */
+import serachIcon from  '../assets/search.png'
+import styles from './MySearchInput.module.css'
+
 export default function MySearchInput() {
     return (
-        <input style={{width:'320px', height:'32px', padding:'8px', borderRadius:'4px'}} placeholder={'请输入搜索笔记名称...'}/>
+        <div className={styles.myInput}>
+            <input style={{width:'320px', height:'32px', padding:'8px', paddingLeft:'32px', borderRadius:'4px'}} placeholder={'请输入搜索笔记名称...'}/>
+            <img src={serachIcon} alt={'search'} style={{width:'22px', height:'22px'}} className={styles.searchIcon} />
+        </div>
     )
 }
